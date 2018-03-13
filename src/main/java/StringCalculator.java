622ccac9a01ac49b6ff2123db22e99dfb123e7cf
Ulysse Rosselet <urosselet@gmail.com>
2018-03-13 10:16:39 +0100

    DelimitersCanBeOfAnyLength passes

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,8 +1,11 @@
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 public class StringCalculator {
 
+  private static final String ANYLENGTH_DELIMITERS_REGEX = "\\[(.*?)\\]";
 
   public static int add(String input) {
     String delimiter = "[,\n]";
@@ -12,11 +15,31 @@ public class StringCalculator {
     if (input.indexOf("//") == 0) {
       int endDelimiterSequence = input.indexOf("\n");
       delimiter = input.substring(2, endDelimiterSequence);
+      if (delimiter.indexOf("[") == 0) {
+        Pattern anyLengthDelimitersPtn = Pattern.compile(ANYLENGTH_DELIMITERS_REGEX);
+        Matcher anyLengthDelimitersMatcher = anyLengthDelimitersPtn.matcher(delimiter);
+        if (anyLengthDelimitersMatcher.find()) {
+          delimiter = anyLengthDelimitersMatcher.group(1);
+          delimiter = escapeSpecialRegexChars(delimiter);
+        }
+      }
       input = input.substring(endDelimiterSequence + 1);
     }
     IntStream numbers = Arrays.stream(input.split(delimiter))
         .mapToInt(Integer::parseInt)
         .filter(value -> value <= 1000);
     return numbers.sum();
+  }
+
+  /**
+   * Escapes all special regex characters
+   * REF: https://stackoverflow.com/a/25853507/2195344
+   *
+   * @param str input
+   * @return string with special regex characters escaped
+   */
+  private static String escapeSpecialRegexChars(String str) {
+    Pattern SPECIAL_REGEX_CHARS = Pattern.compile("[{}()\\[\\].+*?^$\\\\|]");
+    return SPECIAL_REGEX_CHARS.matcher(str).replaceAll("\\\\$0");
   }
 }
